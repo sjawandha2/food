37ca3d2b1ecc4c3d28737bc1f4861d68d6d1a526
Sukhveer Jawandha <sjawandha2@mail.greenriver.edu>
2019-04-15 17:48:54 -0400

    Added routes and forms

--- index.php
+++ index.php
@@ -25,12 +25,35 @@ $f3->route('GET /' , function (){
     $view = new Template();
     echo $view->render('views/home.html');
 });
+// Define a route with a parameter
+$f3 ->route('GET /breakfast/continental' ,function(){
 
-// Define  breakfast route
-$f3 ->route('GET /breakfast/continental' ,function (){
     //Display a views
     $view = new Template();
-    echo $view->render('views/bfast-cont.html');
+   echo $view->render('views/bfast-cont.html');
+});
+
+// Define a route with a parameter
+$f3 ->route('GET /@item' ,function($f3, $params){
+
+    $item = $params['item'];
+    switch ($item)
+    {
+        case 'spagheytti':
+            echo "<h3>I like $item with veggies.</h3>";
+            break;
+        case 'pizza':
+            echo "<h3>Pepperoni or Veggie</h3>";
+            break;
+        case 'taco':
+            echo "<h3>We don't have $item</h3>";
+            break;
+        case 'bagel':
+            $f3->reroute("breakfast/continental");
+        default:
+            $f3->error(404);
+
+    }
 });
 
 // Define  breakfast route
@@ -40,6 +63,25 @@ $f3 ->route('GET /lunch/brunch/buffet' ,function (){
     echo $view->render('views/buffet.html');
 });
 
+
+// Define a route with a parameter
+$f3 ->route('GET /@fname/@lname' ,function($f3, $params){
+
+    //uppercase the first letter of string
+    $fname = ucfirst($params['fname']);
+    $lname = $params['lname'];
+    echo "<h2>Hello, $fname $lname !</h2>";
+    //Display a views
+//    $view = new Template();
+//   echo $view->render('views/bfast-cont.html');
+});
+
+// Define  form1  route
+$f3 ->route('GET /order' ,function (){
+    //Display a views
+    $view = new Template();
+    echo $view->render('views/form1.html');
+});
 
 //Run Fat-Free
 $f3->run();
