a6a366e6364604a2940f2a5ab1a432caff24ec89
Sukhveer Jawandha <sjawandha2@mail.greenriver.edu>
2019-04-15 16:13:09 -0400

    Create Index page

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Name: Sukhveer S Jawandha
+* 4/8/2019
+* 328/food/index.php
+ *
+ * Practice Routing
+ */
+
+//Turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once ('vendor/autoload.php');
+
+//create an instance of the Base class
+$f3 = Base::instance();
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+// Define a default route
+$f3->route('GET /' , function (){
+
+    echo "Food";
+    //Display a view
+//    $view = new Template();
+//    echo $view->render('views/home2.html');
+});
+
+
+//Run Fat-Free
+$f3->run();
